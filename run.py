88c264d05f2d8866bd6c8fb39cfc41917ef18c7a
Alan Smyth <alan_smyth@hotmail.com>
2024-03-30 09:57:56 +0000

    Add welcome and options to chose from

--- run.py
+++ run.py
@@ -2,6 +2,43 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("---------------------------")
-print("Welcome to Health Ireland")
-print("---------------------------")
+def main_menu():
+    """
+    Displays the main menu options for the user
+    to select in order to navigate the application.
+    """
+    print("---------------------------")
+    print("Welcome to Health Ireland")
+    print("---------------------------")
+    print("Please select an option below.\n")
+
+    print("(1) Body Mass Index BMI.")
+    print("(2) Heart Rate.")
+    print("(3) Waist to Hip Ratio ")
+    print("(4) How it works.")
+
+    while True:
+        main_menu_ans = input("\n")
+        if main_menu_ans not in ("1", "2", "3", "4", "5"):
+            print("Invalid input.")
+            print("Please choose an option between 1 and 5.")
+        else:
+            break
+
+    if main_menu_ans == ("1"):
+        bmi()
+    elif main_menu_ans == ("2"):
+        heart_ratio()
+    elif main_menu_ans == ("3"):
+        waist_hip()
+    elif main_menu_ans == ("4"):
+        how_it_work()
+
+def main():
+    """
+    Runs necessary functions at the start of the program.
+    """
+    main_menu()
+
+
+main()
